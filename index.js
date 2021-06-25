76c3c81dd0a614b1e130b6131057d56c48001735
Isa Tekinkaya <isatekinkaya@yahoo.de>
2021-06-25 11:59:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Contutorialuctor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
+  'what does the this keyword mean?', 
+  'What is the Contutorialuctor OO pattern?', 
+  'implementing Blockchain Web API', 
+  'The Test Driven Development Workflow', 
+  'What is NaN and how Can we Check for it', 
+  'What is the difference between stopPropagation and preventDefault?', 
+  'Immutable State and Pure Functions', 
+  'what is the difference between == and ===?', 
+  'what is the difference between event capturing and bubbling?', 
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(sentences){ 
+  
+  let wordsArray = sentences.map(arr =>{
+  let words = arr.split(" ")
+  
+  return words.map(word =>{
+  
+  let firstLetter = word[0].toUpperCase()
+  let rest = word.slice(1).toLowerCase()
+  
+  return `${firstLetter}${rest}`
+  }).join(" ")
+  
+  
+  
+  })
+  return wordsArray
 }
+titleCased(tutorials)
